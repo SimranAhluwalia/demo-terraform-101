cadf86980c4296d5fcca4699e49df0aff5e0bec6
SimranAhluwalia <SimranAhluwalia@users.noreply.github.com>
2020-03-25 12:15:49 -0500

    update

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-terraform {
-  version = ">= 2.27.0"
-}
 provider "aws" {
-}
-
-module "server" {
-  source = "./server"
-
-  num_webs     = var.num_webs
-  identity     = var.identity
-  ami          = var.ami
-  ingress_cidr = var.ingress_cidr
-  public_key   = var.public_key
-  private_key  = var.private_key
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
 
